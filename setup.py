0c90b669c25e0928610d45a9e576bfac93331b12
Fantomas42 <fantomas42@gmail.com>
2013-01-28 15:42:57 +0100

    clean on setup

--- setup.py
+++ setup.py
@@ -4,29 +4,31 @@ from setuptools import find_packages
 
 import zinnia_bootstrap
 
-setup(name='zinnia-theme-bootstrap',
-      version=zinnia_bootstrap.__version__,
+setup(
+    name='zinnia-theme-bootstrap',
+    version=zinnia_bootstrap.__version__,
 
-      description='Bootstrap theme for django-blog-zinnia',
-      long_description=open('README.rst').read(),
+    description='Bootstrap theme for django-blog-zinnia',
+    long_description=open('README.rst').read(),
 
-      keywords='django, blog, weblog, zinnia, theme, skin, bootstrap',
+    keywords='django, blog, weblog, zinnia, theme, skin, bootstrap',
 
-      author=zinnia_bootstrap.__author__,
-      author_email=zinnia_bootstrap.__email__,
-      url=zinnia_bootstrap.__url__,
+    author=zinnia_bootstrap.__author__,
+    author_email=zinnia_bootstrap.__email__,
+    url=zinnia_bootstrap.__url__,
 
-      packages=find_packages(exclude=['demo_zinnia_bootstrap']),
-      classifiers=[
-          'Framework :: Django',
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Web Environment',
-          'Programming Language :: Python',
-          'Intended Audience :: Developers',
-          'Operating System :: OS Independent',
-          'License :: OSI Approved :: BSD License',
-          'Topic :: Software Development :: Libraries :: Python Modules'],
+    packages=find_packages(exclude=['demo_zinnia_bootstrap']),
+    classifiers=[
+        'Framework :: Django',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules'],
 
-      license=zinnia_bootstrap.__license__,
-      include_package_data=True,
-      zip_safe=False)
+    license=zinnia_bootstrap.__license__,
+    include_package_data=True,
+    zip_safe=False
+)
