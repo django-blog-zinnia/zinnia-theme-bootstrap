9aeb7b9782fb6fe3ed19b0c16316ff5ba230c22a
Fantomas42 <fantomas42@gmail.com>
2014-03-22 18:30:45 +0100

    Updating the settings of the demo

--- demo_zinnia_bootstrap/settings.py
+++ demo_zinnia_bootstrap/settings.py
@@ -34,6 +34,8 @@ LANGUAGES = (
     ('es', gettext('Spanish')),
     ('it', gettext('Italian')),
     ('nl', gettext('Dutch')),
+    ('sl', gettext('Slovenian')),
+    ('bg', gettext('Bulgarian')),
     ('hu', gettext('Hungarian')),
     ('cs', gettext('Czech')),
     ('sk', gettext('Slovak')),
@@ -41,8 +43,10 @@ LANGUAGES = (
     ('ru', gettext('Russian')),
     ('pl', gettext('Polish')),
     ('eu', gettext('Basque')),
+    ('he', gettext('Hebrew')),
     ('ca', gettext('Catalan')),
     ('tr', gettext('Turkish')),
+    ('sv', gettext('Swedish')),
     ('hr_HR', gettext('Croatian')),
     ('pt_BR', gettext('Brazilian Portuguese')),
     ('fi_FI', gettext('Finnish (Finland)')),
@@ -55,7 +59,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.middleware.doc.XViewMiddleware',
+    'django.contrib.admindocs.middleware.XViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
@@ -70,7 +74,6 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.i18n',
     'django.core.context_processors.request',
-    'django.core.context_processors.media',
     'django.contrib.messages.context_processors.messages',
     'zinnia.context_processors.version',
 )
